11318db03f16228c1b4c21875dbd3da638c45184
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-29 19:53:16 +0700

    Initial commit

---
+++ src/com/rk/Hello.java
@@ -0,0 +1,10 @@
+package com.rk;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Hello extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Rizky Khapidsyah",80,25);
+    }
+}
